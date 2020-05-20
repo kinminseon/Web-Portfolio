8958cba2f6acc5701acf9085e81c4392331e697d
kinminseon <53294939+kinminseon@users.noreply.github.com>
2020-05-21 01:51:27 +0900

    포트폴리오 파일
    포트폴리오 파일

---
+++ port/nikon/js/custom.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function(){  	
+	$('.mo_btn').on('click',function(){
+		$('#gnbMo').fadeIn();
+		return false;
+	});
+	$('#gnbMo>a').on('click',function(){
+		$('#gnbMo').fadeOut();
+		return false;
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
