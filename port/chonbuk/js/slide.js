1ae3e0dcc44f2023be8b1cdcfe81a0c6d3e3bdb1
kinminseon <53294939+kinminseon@users.noreply.github.com>
2020-05-21 02:14:59 +0900

    Add files via upload

---
+++ port/chonbuk/js/slide.js
@@ -0,0 +1,53 @@
+﻿$(document).ready(function(){
+	$('.con03_text').css('left',-300).each(function(index){
+		$(this).attr('data-index',index);
+	});
+	$('.control_button').each(function(index){
+		$(this).attr('data-index',index);
+	}).click(function(){
+		var index = $(this).attr('data-index');
+		//alert(index);
+		moveSlider(index);
+	});
+	function moveSlider(index){
+		var willMoveLeft = -(index%3*453);
+		$('.slider').animate({left:willMoveLeft},'slow');
+		$('.control_button[data-index='+index+']').addClass('active');
+		$('.control_button[data-index!='+index+']').removeClass('active');
+		$('.con03_text[data-index='+index+']').show().animate({left:56},'slow');
+		$('.con03_text[data-index!='+index+']').show().animate({left:-300},'slow');
+	}
+	var randomNumber = Math.round(Math.random()*1);
+	moveSlider(randomNumber);
+    
+    //swiper slide
+    var swiper = new Swiper('.swiper-container',{
+        effect:'fade',
+        loop:'true',
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+          },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+    });
+	
+}); 
+
+
+
+
+
+
+
+
+
+
---
+++ port/chonbuk/sub5/js/custom.js
@@ -0,0 +1,32 @@
+﻿jQuery(document).ready(function(){
+	var tbody = $('#tbody');
+	$.ajax({
+		url:'data.xml',
+		dataType:'xml',
+		success:function(xml){
+			var list = $(xml);
+			list.find('row').each(function(){
+				var row = $(this);
+				var text_cell1 = row.find('cell1').text();
+				var text_cell2 = row.find('cell2').text();
+				var text_cell3 = row.find('cell3').text();
+				var text_cell4 = row.find('cell4').text();
+				var text_cell5 = row.find('cell5').text();
+				var tr = $('<div class="row"></div>');
+				var cell1 = $('<div class="cell1 cell"></div>').text(text_cell1);
+				var cell2 = $('<div class="cell2 cell"></div>').text(text_cell2);
+				var cell3 = $('<div class="cell3 cell"></div>').text(text_cell3);
+				var cell4 = $('<div class="cell4 cell"></div>').text(text_cell4);
+				var cell5 = $('<div class="cell5 cell"></div>').text(text_cell5);
+				tr.append(cell1);
+				tr.append(cell2);
+				tr.append(cell3);
+				tr.append(cell4);
+				tr.append(cell5);
+				tbody.append(tr);
+			});
+		}
+	});
+});
+
+
